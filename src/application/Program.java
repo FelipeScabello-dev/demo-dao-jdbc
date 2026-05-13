f5947ccc7d1c7b3d944fb4972687810dc01fdd26
FelipeScabello-dev <scabellofelipe@gmail.com>
2026-05-13 18:05:00 -0300

    reusando instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,8 @@ public class Program {
         Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
 
         SellerDao SellerDao = DaoFactory.creatSellerDao();
-
+        
+        System.out.println("=== TEST 1: seller findByid ====");
         seller = SellerDao.findByid(3);
 
         System.out.println(seller);
